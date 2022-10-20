d5bcd52a34b32e4b80b7165c58bdb6213a340a6b
goranagolubovic <45577207+goranagolubovic@users.noreply.github.com>
2022-10-20 20:07:49 +0200

    created spring boot project

---
+++ sarlota-api/src/main/java/sarlota/SarlotaApiApplication.java
@@ -0,0 +1,13 @@
+package sarlota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarlotaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarlotaApiApplication.class, args);
+	}
+
+}
