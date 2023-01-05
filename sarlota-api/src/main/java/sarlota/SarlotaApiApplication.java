bbf4cedf213761b62e2ad63a254bab6a2049258c
stekap000 <80646042+stekap000@users.noreply.github.com>
2023-01-05 22:53:50 +0100

    Update SarlotaApiApplication.java

--- sarlota-api/src/main/java/sarlota/SarlotaApiApplication.java
+++ sarlota-api/src/main/java/sarlota/SarlotaApiApplication.java
@@ -2,12 +2,8 @@ package sarlota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//========================================================
-// DODAN 'exclude' SAMO RADI TESTIRANJA
-//========================================================
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class SarlotaApiApplication {
 
 	public static void main(String[] args) {
